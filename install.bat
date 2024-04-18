d1b7dd727c2e1d9981c9ea11fee6acd48b3dfa3b
athish-ns <159168028+athish-ns@users.noreply.github.com>
2024-04-18 12:53:44 +0530

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Install required Python modules
+pip install DateTime
+pip install email
+pip install webbrowser
+pip install numpy
+pip install pyttsx3
+pip install SpeechRecognition
+pip install requests
+pip install beautifulsoup4
+pip install pyautogui
+pip install random2
+pip install plyer
+pip install pygame
+pip install speedtest-cli
+pip install openai
+pip install json
+pip install smtplib
+pip install imapclient
+
+echo All required Python modules installed successfully.
+
+pause
---
+++ main/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.2 (trial), 000000, 2024-04-18T10:16:31.442955
+from .pyarmor_runtime import __pyarmor__
